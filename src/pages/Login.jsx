68b878a8a03768b7d5fc033a95d5bfa7a808f96a
Letícia França <leeffrance@gmail.com>
2026-02-18 20:05:28 -0300

    Fix: Correções para melhorar a acessibilidade da página

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,19 +9,18 @@ import SocialButton from "../components/SocialButton";
 const Login = () => {
 
     return (
-        <div className="min-h-screen flex items-center justify-center p-4 bg-[#FFE6C9]">
+        <main className="min-h-screen flex items-center justify-center p-4 bg-[#FFE6C9]">
             <div className="flex flex-col lg:flex-row max-w-7xl w-full bg-white rounded-2xl overflow-hidden shadow-[0_0_45px_#E1C2A8]">
                 
-
                 {/* image */}
                 <div className="w-full h-56 sm:h-72 lg:h-auto lg:w-3/5 relative lg:p-2 bg-white">
-                    <img src={LoginImage} alt="" className="w-full h-full object-cover"/>
+                    <img src={LoginImage} alt="Skull using a laptop with the phrase Turn your ideas into reality. Start for free and get attractive offers from the community." className="w-full h-full object-cover"/>
                 </div>
 
                 {/* login form */}
                 <div className="flex-1 p-8 shadow-xl sm:p-12 lg:p-16 flex flex-col justify-center">
-                    <img src={Icon} alt="" className="w-12 pt-16"/>
-                    <h2 className="text-[#525252] text-2xl sm:text-3xl font-bold mt-6">Login to your Account</h2>
+                    <img src={Icon} alt="Company logo" className="w-12 pt-16"/>
+                    <h1 className="text-[#525252] text-2xl sm:text-3xl font-bold mt-6">Login to your Account</h1>
                     <span className="text-[#525252] text-sm sm:text-base mb-6 block">See what is going on with your business</span>
 
                     {/* google buttom */}
@@ -34,12 +33,11 @@ const Login = () => {
                     
                     <form className="space-y-5">
                         {/* email */}
-                        <InputField label="Email" type="text" id="email" placeholder="mail@abc.com"/>
+                        <InputField label="Email" type="email" id="email" placeholder="mail@abc.com"/>
          
                         {/* password */}
                         <InputField label="Password" type="password" id="password" placeholder="*****************"/>
 
-                        
                         {/* forgot password link */}
                         <div className="flex justify-between items-center">
                             <div className="text-left">
@@ -59,7 +57,6 @@ const Login = () => {
                                 </a>
                             </div>
                         </div>
-                        
 
                         {/* login buttom */}
                         <PrimaryButton>Login</PrimaryButton>
@@ -75,7 +72,7 @@ const Login = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
 
